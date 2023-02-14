270798282fade34238c135a73623416c26682a6d
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-02-14 13:36:46 +0900

    add Eslint

--- app/javascript/components/Editor.jsx
+++ app/javascript/components/Editor.jsx
@@ -1,37 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import EventList from './EventList';
 // 全体の処理の概要として、APIに接続し、イベントリストを取得し、それをEventListに渡してWebページ上に表示できるようにしている。
 const Editor = () => {
-    const [events, setEvents] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-  
-    useEffect(() => {
-      const fetchData = async () => { // fetchData関数は、/api/eventsエンドポイントにFetchAPIでアクセスする。有効なJSONレスポンスを返したら、ステートのevents変数に保存する。
-        try {
-          const response = await window.fetch('/api/events');
-          if (!response.ok) throw Error(response.statusText);
-          const data = await response.json();
-          setEvents(data);
-        } catch (error) {
-          setIsError(true);
-          console.error(error);
-        }
-  
-        setIsLoading(false);
-      };
-  
-      fetchData();
-    }, []);
-    // データのフェッチが終了したら、isLoadingをfalseに設定される。
-    return (
-      <>
-        <Header />
-        {isError && <p>Something went wrong. Check the console.</p>}
-        {isLoading ? <p>Loading...</p> : <EventList events={events} />}
-      </>
-    );
-  };
-  
-  export default Editor;
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    // fetchData関数は、/api/eventsエンドポイントにFetchAPIでアクセスする。有効なJSONレスポンスを返したら、ステートのevents変数に保存する。
+
+    const fetchData = async () => { 
+      try {
+        const response = await window.fetch('/api/events');
+        if (!response.ok) throw Error(response.statusText);
+        // console.log(response);
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  // データのフェッチが終了したら、isLoadingをfalseに設定される。
+  return (
+    <>
+      <Header />
+      {isError && <p>Something went wrong. Check the console.</p>}
+      {isLoading ? <p>Loading...</p> : <EventList events={events} />}
+    </>
+  );
+};
+
+export default Editor;
--- app/javascript/components/EventList.jsx
+++ app/javascript/components/EventList.jsx
@@ -1,39 +1,37 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 /* 処理の全体の概要は、イベントオブジェクトの配列をeventsパラメータが受け取り、renderEventsでソート後、
 各イベントリスト項目をレンダリング */
 const EventList = ({ events }) => {
-    const renderEvents = (eventArray) => {
-        // 配列を日付の降順でソートしてから、各イベントのリスト項目をレンダリング
-      eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
-  
-      return eventArray.map((event) => (
-        <li key={event.id}>
-          {event.event_date}
-          {' - '}
-          {event.event_type}
-        </li>
-      ));
-    };
-  
-    return (
-      <section>
-        <h2>Events</h2>
-        <ul>{renderEvents(events)}</ul>
-      </section>
-    );
+  const renderEvents = (eventArray) => {
+    // 配列を日付の降順でソートしてから、各イベントのリスト項目をレンダリング
+    eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+    return eventArray.map((event) => (
+      <li key={event.id}>
+        {event.event_date}
+        {' - '}
+        {event.event_type}
+      </li>
+    ));
   };
+  return (
+    <section>
+      <h2>Events</h2>
+      <ul>{renderEvents(events)}</ul>
+    </section>
+  );
+};
   // イベントリストの型情報定義
-  EventList.propTypes = {
-    events: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number,
-      event_type: PropTypes.string,
-      event_date: PropTypes.string,
-      title: PropTypes.string,
-      speaker: PropTypes.string,
-      host: PropTypes.string,
-      published: PropTypes.bool,
-    })).isRequired,
-  };
-  
-  export default EventList;
+EventList.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    event_type: PropTypes.string,
+    event_date: PropTypes.string,
+    title: PropTypes.string,
+    speaker: PropTypes.string,
+    host: PropTypes.string,
+    published: PropTypes.bool,
+  })).isRequired,
+};
+
+export default EventList;
