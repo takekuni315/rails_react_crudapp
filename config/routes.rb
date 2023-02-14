062a9715a458e09a40bb03e73694a3d077e68b7c
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-02-14 14:08:58 +0900

    Reactアプリで使うルーティングをRailsに知らせる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root to:'site#index'
+  root to: redirect('/events')
 
-  namespace :api do 
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
+  get 'events/:id', to: 'site#index'
+  get 'events/:id/edit', to: 'site#index'
+
+  namespace :api do
     resources :events, only: %i[index show create destroy update]
   end
 end
