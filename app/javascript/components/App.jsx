7bd8747e3f1e032af42abeabece8945b34761f56
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-02-14 14:12:55 +0900

    Routeコンポーネントでブラウザでevents/で始まるURLの表示をレンダリングする処理を実装

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
 import Editor from './Editor';
-const App = () => <Editor />;
+
+const App = () => (
+  <Routes>
+    <Route path="events/*" element={<Editor />} />
+  </Routes>
+);
 
 export default App;
