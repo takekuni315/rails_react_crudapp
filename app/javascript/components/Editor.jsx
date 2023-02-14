b9a39d7a1bb32e34d5141f185e3fff6657404d87
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-02-14 14:16:01 +0900

    イベントidと一致したイベントが表示できるようにする処理を実装

--- app/javascript/components/Editor.jsx
+++ app/javascript/components/Editor.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import EventList from './EventList';
+import Event from './Event';
 // 全体の処理の概要として、APIに接続し、イベントリストを取得し、それをEventListに渡してWebページ上に表示できるようにしている。
 const Editor = () => {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  // const [isError, setIsError] = useState(false);
   useEffect(() => {
     // fetchData関数は、/api/eventsエンドポイントにFetchAPIでアクセスする。有効なJSONレスポンスを返したら、ステートのevents変数に保存する。
 
-    const fetchData = async () => { 
+    const fetchData = async () => {
       try {
         const response = await window.fetch('/api/events');
         if (!response.ok) throw Error(response.statusText);
@@ -28,8 +32,17 @@ const Editor = () => {
   return (
     <>
       <Header />
-      {isError && <p>Something went wrong. Check the console.</p>}
-      {isLoading ? <p>Loading...</p> : <EventList events={events} />}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <EventList events={events} />
+
+          <Routes>
+            <Route path=":id" element={<Event events={events} />} />
+          </Routes>
+        </>
+      )}
     </>
   );
 };
