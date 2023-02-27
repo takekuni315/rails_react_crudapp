99c702d68ec0b62477569da6fc8a4531e2201f8a
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-02-27 17:20:35 +0900

    App.cssをReactコンポーネントと同じ階層に配置

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
 import Editor from './Editor';
+import './App.css';
 
 const App = () => (
   <Routes>
