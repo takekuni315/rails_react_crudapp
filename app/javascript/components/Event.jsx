b0255dc02cbb16802f4bcb0f1b0cdb386312cc65
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-03-02 15:22:37 +0900

    イベントリストのデータのclassNameを定義

--- app/javascript/components/Event.jsx
+++ app/javascript/components/Event.jsx
@@ -8,7 +8,7 @@ const Event = ({ events }) => {
   const event = events.find((e) => e.id === Number(id));
 
   return (
-    <>
+    <div className="eventContainer">
       <h2>
         {event.event_date}
         {' - '}
@@ -34,7 +34,7 @@ const Event = ({ events }) => {
           <strong>Published:</strong> {event.published ? 'yes' : 'no'}
         </li>
       </ul>
-    </>
+    </div>
   );
 };
 
