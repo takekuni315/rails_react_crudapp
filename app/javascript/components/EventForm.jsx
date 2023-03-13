23cf7afd9a7f2c29508b902d883a4b0b647ce3f5
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-03-13 17:05:16 +0900

    isEmptyObject関数とvalidateEvent関数をそれぞれヘルパー関数として分離

--- app/javascript/components/EventForm.jsx
+++ app/javascript/components/EventForm.jsx
@@ -23,33 +23,33 @@ const EventForm = () => {
     setEvent({ ...event, [name]: value });
   };
 
-  const validateEvent = () => {
-    const errors = {};
+  // const validateEvent = () => {
+  //   const errors = {};
 
-    if (event.event_type === '') {
-      errors.event_type = 'You must enter an event type';
-    }
+  //   if (event.event_type === '') {
+  //     errors.event_type = 'You must enter an event type';
+  //   }
 
-    if (event.event_date === '') {
-      errors.event_date = 'You must enter a valid date';
-    }
+  //   if (event.event_date === '') {
+  //     errors.event_date = 'You must enter a valid date';
+  //   }
 
-    if (event.title === '') {
-      errors.title = 'You must enter a title';
-    }
+  //   if (event.title === '') {
+  //     errors.title = 'You must enter a title';
+  //   }
 
-    if (event.speaker === '') {
-      errors.speaker = 'You must enter at least one speaker';
-    }
+  //   if (event.speaker === '') {
+  //     errors.speaker = 'You must enter at least one speaker';
+  //   }
 
-    if (event.host === '') {
-      errors.host = 'You must enter at least one host';
-    }
+  //   if (event.host === '') {
+  //     errors.host = 'You must enter at least one host';
+  //   }
 
-    return errors;
-  };
+  //   return errors;
+  // };
 
-  const isEmptyObject = (obj) => Object.keys(obj).length === 0;
+  // const isEmptyObject = (obj) => Object.keys(obj).length === 0;
 
   const renderErrors = () => {
     if (isEmptyObject(formErrors)) {
