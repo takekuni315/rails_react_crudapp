5203784c4a8b3b59286ba9b652a7248d2edd1af6
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-03-30 14:59:29 +0900

    endの改行

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,25 +1,25 @@
 class Api::EventsController < ApplicationController
     before_action :set_event, only: %i[show update destroy]
-  
+
     def index
       @events = Event.all
       render json: @events
     end
-  
+
     def show
       render json: @event
     end
-  
+
     def create
       @event = Event.new(event_params)
-    
+
       if @event.save
         render json: @event, status: :created
       else
         render json: @event.errors, status: :unprocessable_entity
       end
     end
-  
+
     def update
       if @event.update(event_params)
         render json: @event, status: :ok
@@ -27,16 +27,16 @@ class Api::EventsController < ApplicationController
         render json: @event.errors, status: :unprocessable_entity
       end
     end
-  
+
     def destroy
       @event.destroy
     end
-  
+
     private
     def set_event
       @event = Event.find(params[:id])
     end
-    
+
       # 許可するパラメータをprivateメソッドでカプセル化
     def event_params
       params.require(:event).permit(
--- db/schema.rb
+++ db/schema.rb
@@ -21,5 +21,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_082337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
