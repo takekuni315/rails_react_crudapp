5bd25b7fa97e7be5c7befa8ab3df516fd8bcf26b
Kunitoshi Takeuchi <ktakeuchi@newcom07.net>
2023-03-30 14:59:13 +0900

    エラーメッセージを日本語に修正。また、Dateオブジェクトの受け渡しの処理を実装

--- app/javascript/helpers/helpers.js
+++ app/javascript/helpers/helpers.js
@@ -4,24 +4,32 @@ export const validateEvent = (event) => {
   const errors = {};
 
   if (event.event_type === '') {
-    errors.event_type = 'You must enter an event type';
+    errors.event_type = 'イベントタイプを入力してください。';
   }
 
   if (event.event_date === '') {
-    errors.event_date = 'You must enter a valid date';
+    errors.event_date = '有効な日付を入力してください。';
   }
 
   if (event.title === '') {
-    errors.title = 'You must enter a title';
+    errors.title = 'イベントのタイトルを入力してください。';
   }
 
   if (event.speaker === '') {
-    errors.speaker = 'You must enter at least one speaker';
+    errors.speaker = 'スピーチする人の名前を入力してください。';
   }
 
   if (event.host === '') {
-    errors.host = 'You must enter at least one host';
+    errors.host = '主催者の名前を入力してください。';
   }
 
   return errors;
+};
+// Dateオブジェクトを受け取って、YYYY-MM_DD形式の文字を返す
+export const formatDate = (d) => {
+  const YYYY = d.getFullYear();
+  const MM = `0${d.getMonth() + 1}`.slice(-2);
+  const DD = `0${d.getDate()}`.slice(-2);
+
+  return `${YYYY} - ${MM} - ${DD}`;
 };
